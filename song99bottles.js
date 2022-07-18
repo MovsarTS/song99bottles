b4b3ce38ec91d3793f518764d1e130e0149fcd16
MovsarTS <mvs451@mail.ru>
2022-07-18 10:08:19 +0300

    Немного доработал функцию

--- song99bottles.js
+++ song99bottles.js
@@ -4,6 +4,8 @@ function generateSingleStanza(number) {
         console.log(`${number} бутылок пива на стене, ${number} бутылок пива! Возьми одну, пусти по кругу, ${number - 1} бутылок пива на стене`)
         } else if (number % 10 === 1) {
         console.log(`${number} бутылка пива на стене, ${number} бутылка пива! Возьми одну, пусти по кругу, ${number - 1} бутылок пива на стене`)
+        } else if (number === 15) {
+        console.log(`${number} бутылок пива на стене, ${number} бутылок пива! Возьми одну, пусти по кругу, ${number - 1} бутылок пива на стене`)
         } else if (number % 10 === 2) {
         console.log(`${number} бутылки пива на стене, ${number} бутылки пива! Возьми одну, пусти по кругу, ${number - 1} бутылка пива на стене`)
         } else if (number % 10 === 3 || number % 10 === 4) {
